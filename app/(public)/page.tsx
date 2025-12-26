e75e96c8ebbc13cb7c4521f9f7c4d9494f5d33b0
Ritika Shrestha <shrestharitika998@gmail.com>
2025-12-26 20:20:35 +0545

    (public) page added

---
+++ app/(public)/page.tsx
@@ -0,0 +1,47 @@
+// export default function HomePage() {
+//   return (
+//     <div className="h-[400px] flex items-center justify-center text-gray-400">
+//       Home Page Content (Dummy)
+//     </div>
+//   );
+// }
+
+
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <section className="grid md:grid-cols-2 gap-10 items-center">
+      
+//       <div>
+//         <h1 className="text-3xl font-bold mb-4">
+//           Welcome to NovaCash
+//         </h1>
+//         <p className="text-foreground/70 mb-6">
+//           Send, receive, and manage your money easily with NovaCash.
+//         </p>
+//       </div>
+
+//       <div>
+//         <Image
+//           src="/digitalwalletlogin.png"
+//           alt="Digital Wallet"
+//           width={500}
+//           height={400}
+//           className="mx-auto"
+//         />
+//       </div>
+
+//     </section>
+//   );
+// }
+
+
+export default function HomePage() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-20">
+      {/* Empty section as per design */}
+    </div>
+  );
+}
+
