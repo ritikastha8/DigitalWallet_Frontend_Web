56402a3f802ea23e9a6ba23e5a3a15f0e2ed3557
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-02 21:50:17 +0545

    Auth dashboard updated

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold">Dashboard</h1>
-      <p className="mt-2 text-foreground/70">
-        This is a dashboard page after login.
-      </p>
-    </div>
-  );
-}
+// export default function DashboardPage() {
+//   return (
+//     <div>
+//       <h1 className="text-2xl font-semibold">Dashboard</h1>
+//       <p className="mt-2 text-foreground/70">
+//         This is a dashboard page after login.
+//       </p>
+//     </div>
+//   );
+// }
