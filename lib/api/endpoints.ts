9fec6aab18e85affeb0094454dd012d161b0759f
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-02 21:56:20 +0545

    Updated endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -5,5 +5,12 @@ export const API ={
     AUTH:{
         LOGIN:'/api/auth/login',
         REGISTER:'/api/auth/register',
+        WHOAMI: '/api/auth/whoami',
+        UPDATEPROFILE: '/api/auth/upload-profile',
+    },
+    ADMIN:{
+        USER:{
+            CREATE: '/api/admin/users/',
+        }
     }
 }
