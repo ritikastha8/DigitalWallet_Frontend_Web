3a8e1c0c0af46ea67270507bea64890387cb86ee
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-02 22:48:51 +0545

    Added navbar

---
+++ app/user/_components/NavBar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-[#EC9444]">
+      <div className="mx-auto max-w-7xl px-4">
+        <ul className="flex h-10 items-center gap-12 text-sm text-white"> {/* increased gap */}
+          <li><Link href="/">Homepage</Link></li>
+          <li><Link href="#">Send Money</Link></li>
+          <li><Link href="#">Topup</Link></li>
+          <li><Link href="#">Load Money</Link></li>
+          <li><Link href="#">Transaction History</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
