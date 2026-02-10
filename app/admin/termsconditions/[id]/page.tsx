f10cd9a423848cdc2f8dbaeb93fac0537f14c52e
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-11 02:24:16 +0545

    Create TermsCondition detail page with back and edit links

---
+++ app/admin/termsconditions/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { handleGetOneTermsCondition } from "@/lib/actions/admin/termscondition-action";
+import Link from "next/link";
+export default async function Page({
+    params
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    const response = await handleGetOneTermsCondition(id);
+    if (!response.success) {
+        throw new Error(response.message || 'Failed to load terms condition');
+    }
+
+    return (
+        <div>
+            <Link href="/admin/termsconditions" className="text-[#D07522] hover:underline">&lt; Back</Link>
+            <Link href={`/admin/termsconditions/${id}/edit`} className="text-green-600 hover:underline ml-4">Edit Terms Condition</Link>
+             <div className="p-4 flex items-center justify-between mt-6 mb-4">
+                <h1 className="font-sans text-gray-600 font-semibold text-4xl" style={{ fontFamily: 'Nunito Sans' }}>Terms Condition Detail</h1>
+            </div>
+               <div className="border border-gray-300 rounded-lg p-6 space-y-2 w-200 ">
+                <p><strong> Title:</strong> {response.data.title}</p>
+                <p><strong>Description:</strong> {response.data.description}</p>
+                {/* Add more TermsCondition details as needed */}
+            </div>
+        </div>
+    );
+}
