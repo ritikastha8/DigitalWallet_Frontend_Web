56412c052ac170ad003cd310e2c5efc76f81ecd5
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-11 01:36:00 +0545

    add Reset Password page with token validation and ResetPasswordForm

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,35 @@
+import ResetPasswordForm from "../_components/ResetPasswordForm";
+
+
+export default async function Page({
+    searchParams
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+    const query = await searchParams;
+    const token = query.token as string | undefined;
+    if(!token){
+        throw new Error('Invalid or missing token');
+    }
+
+    return (
+        <div className="w-full max-w-md mx-auto">
+            <div className="text-center mb-6">
+            <h1 className="text-2xl font-bold text-[#D07522]">Reset Password</h1>
+            <p className="text-sm text-gray-500 mt-1">Enter your new password</p>
+            </div>
+            <ResetPasswordForm token={token} />
+        </div>
+
+            // <div className="w-full max-w-md mx-auto">
+            //         <div className="text-center mb-6">
+            //             <h1 className="text-2xl font-bold text-[#D07522]">Log In</h1>
+            //             <p className="text-sm text-gray-500 mt-1">Sign in to your account</p>
+            //         </div>
+            //         <LoginForm />
+            //     </div>
+
+
+
+    );
+}
