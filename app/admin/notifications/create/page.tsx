284b6bda9c6a3ec724b1513dda7994feeac72904
Ritika Shrestha <shrestharitika998@gmail.com>
2026-02-11 02:11:00 +0545

    Add notification create

---
+++ app/admin/notifications/create/page.tsx
@@ -0,0 +1,9 @@
+import CreateNotificationForm from "../_components/CreateNotificationForm";
+
+export default function Page() {
+    return (
+        <div>
+            <CreateNotificationForm/>
+        </div>
+    );
+}
